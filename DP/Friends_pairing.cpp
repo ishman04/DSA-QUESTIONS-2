71740f32a0e269a56dbe7a3b3de759b6396aa70a
Ishman Jeet Singh <ishmanalagh@gmail.com>
2024-07-19 20:58:57 +0530

    dp

--- DP/Friends_pairing.cpp
+++ DP/Friends_pairing.cpp
@@ -10,7 +10,8 @@ vector<int> dp;
 int f(int n){
     if(n==1) return 1;
     if(n==2) return 2;
-    return f(n-1) + (n-1)*f(n-2);
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=f(n-1) + (n-1)*f(n-2);
 }
 int main(){
     int n;
