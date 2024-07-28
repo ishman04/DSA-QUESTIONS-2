45eedd706917465704da586c694294994e04616d
Ishman Jeet Singh <ishmanalagh@gmail.com>
2024-07-28 13:04:35 +0530

    dp

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -1,18 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<int>> dp;
-string ans="";
-int f(string &str1, string &str2, int i,int j,string out){ 
-    if(ans.size()<out.size()) ans=out;  
-    if(i<0 || j<0) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    return dp[i][j] = max({( (str1[i]==str2[j]) ? 1+f(str1,str2,i-1,j-1,out+str1[i]) : INT_MIN ),f(str1,str2,i-1,j,out),f(str1,str2,i,j-1,out)});
-}
-int main(){
-    string s1,s2;
-    cin>>s1>>s2;
-    dp.resize(1005,vector<int> (1005,-1));
-    f(s1,s2,s1.length()-1,s2.length()-1,"");
-    reverse(ans.begin(),ans.end());
-    cout<<ans;
-}
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int f(string &s1,string &s2,int i,int j){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if (s1[i] == s2[j]){
+            return dp[i][j] = 1 + f(s1, s2, i - 1, j - 1);
+        } else {
+            return dp[i][j] = max(f(s1, s2, i - 1, j), f(s1, s2, i, j - 1));
+        }
+
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        dp.resize(10005,vector<int>(1005,-1));
+        return f(text1,text2,text1.length()-1,text2.length()-1);
+    }
+};
