4d524cb065959cd66dd1e373d9794962ff174216
Ishman Jeet Singh <ishmanalagh@gmail.com>
2024-07-28 22:07:13 +0530

    dp

--- DP/tempCodeRunnerFile.cpp
+++ DP/tempCodeRunnerFile.cpp
@@ -1,16 +1,19 @@
-
-    while(n>=0){
-        int x=k%10;
-        v.push_back(x);
-        k=k/10;
-    }
-    for(int i=0;i<v.size()-1;i++){
-        return 1+min(f(n-v[i]),f(n-v[i+1]));
-    }
-    return 0;
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>>dp;
+int f(int n,int w,vector<pair<int,int>> &items,int i){
+    if(i==n || w==0) return 0;
+    if(dp[i][w]!=-1) return dp[i][w];
+    if(items[i].first>w) return 0;
+    return dp[i][w] = max(items[i].second+f(n,w-items[i].first,items,i+1),f(n,w,items,i+1));
 }
 int main(){
-    int n;
-    cin>>n;
-    cout<<f(n);
+int n,W;
+cin>>n>>W;
+dp.resize(n+1,vector<int>(W+1,-1));
+vector<pair<int,int>>v(n);
+for(int i=0;i<n;i++){
+    cin>>v[i].first>>v[i].second;
+}
+cout<< f(n,W,v,0);
 }
