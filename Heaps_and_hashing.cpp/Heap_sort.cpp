bd8c7c6c3d7520ca19c7c8c27a0823ff61ddb84f
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-01-19 12:06:23 +0530

    Heaps

---
+++ Heaps_and_hashing.cpp/Heap_sort.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// The functions should be written in a way that array become sorted
+// in increasing order when heapSort() is called.
+
+class Solution {
+  public:
+    // Function to sort an array using Heap Sort.
+    void heapify(vector<int>& arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+
+    void heapSort(vector<int>& arr) {
+        int n = arr.size();
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        obj.heapSort(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        // cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
