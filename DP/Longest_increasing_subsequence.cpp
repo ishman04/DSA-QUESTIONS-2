12c096b84737f3fbd0eff2089df2d7d078486e4c
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-03-29 11:51:59 +0530

    DP

--- DP/Longest_increasing_subsequence.cpp
+++ DP/Longest_increasing_subsequence.cpp
@@ -1,25 +1,58 @@
-#include<bits/stdc++.h>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
+
+
+// } Driver Code Ends
+
 class Solution {
-public:
-    vector<int> dp;
-    int f(vector<int>& nums,int n,int i){
-        if(dp[i]!=-1) return dp[i];
-        int mx=1;
-        for(int j=0;j<=i;j++){
-            if(nums[j]<nums[i]){
-                mx=max(mx,1+f(nums,n,j));
-            }
+  public:
+    vector<vector<int>> dp;
+    int f(int n,int prev, int i, vector<int>& arr){
+        if(i==n) return 0;
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        int take = INT_MIN;
+        int nottake = INT_MIN;
+        if(prev==-1 || arr[i]>arr[prev]){
+            take = 1 + f(n,i,i+1,arr);
         }
-        return dp[i]=mx;
-    }   
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        dp.resize(n,-1);
-        int ans=-1;
-        for(int i=0;i<n;i++){
-            ans=max(ans,f(nums,n,i));
-        }
-        return ans;
+        nottake = f(n,prev,i+1,arr);
+        return dp[i][prev+1] = max(take,nottake);
+    }
+    int lis(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        dp.resize(n+2,vector<int>(n+2,-1));
+        return f(n,-1,0,arr);
     }
 };
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
