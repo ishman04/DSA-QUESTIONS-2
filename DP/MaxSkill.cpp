63ef5aa7ab03601deea9d34b659ba93687bbe210
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-03-29 18:28:36 +0530

    DP

--- DP/MaxSkill.cpp
+++ DP/MaxSkill.cpp
@@ -7,22 +7,24 @@ using namespace std;
 
 class Solution {
   public:
+    vector<vector<int>> dp;
     int f(int i,int j,vector<int> &arr){
         if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
         int ans = INT_MIN;
         for(int k=i;k<=j;k++){
             int sum = f(i,k-1,arr) + f(k+1,j,arr) + arr[i-1]*arr[j+1]*arr[k];
             ans = max(ans,sum);
         }
-        return ans;
+        return dp[i][j]= ans;
     }
     int maxSkill(vector<int> &arr) {
         // code here
         int n = arr.size();
+        dp.resize(n+2,vector<int>(n+2,-1));
         arr.insert(arr.begin(),1);
         arr.push_back(1);
         return f(1,n,arr);
-        
     }
 };
 
