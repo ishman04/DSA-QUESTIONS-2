28140e559d3e7d342e66a6d87fbaf8cff1aa2464
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-08-10 13:57:04 +0530

    mixed

---
+++ Mixed 40 questions/LL_reverse.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+}
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* Next = nullptr;
+        ListNode* prev = nullptr;
+        while(curr){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+    }
+};
